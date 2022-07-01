bba451c3fd372d16409c7431373c3cfe99c3e0f7
Jake Clarke <jake.t.clarke@gmail.com>
2022-07-01 14:23:04 +1000

    first run

--- sed-victoria.py
+++ sed-victoria.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import pandas as pd
 import functools as ft
+import numpy as np
 
 def get_source_table(tableid):
     return f'census-data{os.sep}SA1{os.sep}AUS{os.sep}2021Census_{tableid}_AUST_SA1.csv'
@@ -41,7 +42,13 @@ def combine_tables(tableids, total_name):
 
     # merge together on sa1 column
     merge = ft.reduce(lambda left, right: pd.merge(left, right, on='SA1_CODE_2021'), dataframes)
-    merge.to_csv(get_destination_table(tableid, total_name), index=False)
+
+    # some of the dataframes contain '..'
+    # will check with abs but assume this means data not provided on small columns (e.g, M_Ptn_in_RM_0_14 in 27a)
+    # replacing with nan
+    merge.replace({'..': np.nan}, regex=True, inplace=True)
+
+    merge.to_csv(get_destination_table(tableid, total_name), index=False, na_rep='NaN') #na_rep preserves the nan in csv dump
 
     print(f'output table {tableid}\r\n')
 
@@ -65,9 +72,9 @@ for tableid in ('G05','G18','G22','G23','G25'):
 # tables split across multiple files
 # we combine together so the weighter has the denominator columnn for all of them
 combine_tables(['G09A', 'G09B', 'G09C', 'G09D', 'G09E', 'G09F', 'G09G', 'G09H'], total_name)
-combine_tables(['G13A', 'G13B', 'G13C', 'G13D', 'G13E'], total_name)
+combine_tables(['G13A', 'G13B', 'G13C', 'G13D', 'G13E'], total_name) ## come and check this one something odd going on
 combine_tables(['G16A', 'G16B'], total_name)
 combine_tables(['G17A', 'G17B', 'G17C'], total_name)
 combine_tables(['G19A', 'G19B', 'G19C'], total_name)
 combine_tables(['G20A', 'G20B'], total_name)
-combine_tables(['G27A', 'G27B'], total_name)
+combine_tables(['G27A', 'G27B'], total_name) # come back and check
