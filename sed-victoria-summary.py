c95cffb6ff7a5f0194d878007376d6db103613a0
Jake Clarke <jake.t.clarke@gmail.com>
2022-07-06 16:46:18 +1000

    add summary output files

--- sed-victoria-summary.py
+++ sed-victoria-summary.py
@@ -10,7 +10,7 @@ import numpy as np
 # combine all unpivoted data into one file
 dir = 'census-data-sed-victoria'
 make_directorytree_if_not_exists( f'{dir}{os.sep}summary')
-unpivot_merge_filepath = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_unpivoted.csv'
+unpivot_merge_filepath = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_unpivoted_merged.csv'
 
 df = pd.DataFrame()
 # if file does not exist, create
@@ -32,15 +32,43 @@ if not os.path.isfile(unpivot_merge_filepath):
     # reshape and order
     df_unpivot_all = df[['table','census_variable','district','value']].sort_values(['table','census_variable','district'])
     df_unpivot_all.to_csv(unpivot_merge_filepath, index=False, na_rep="Null")
-    print(df_unpivot_all)
 
-df = pd.read_csv(unpivot_merge_filepath, na_values=['Null','NaN','nan','Nan'])
-pivot = df.pivot(index='district', columns=['table', 'census_variable'], values='value')
-pivot.to_csv( f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022.csv', na_rep="Null")
+pivot_questions_filepath = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_wide_combined.csv'
+if not os.path.isfile(pivot_questions_filepath):
+    df = pd.read_csv(unpivot_merge_filepath, na_values=['Null','NaN','nan','Nan'])
+    pivot = df.pivot(index='district', columns=['table', 'census_variable'], values='value')
+    pivot.to_csv( pivot_questions_filepath, na_rep='Null')
 
-# # repivot
-# transform = pd.pivot_table(data=df, index=['table','census_variable'])
-# transform = transform.reset_index()
-# transform.to_csv( f'{path}{os.sep}summary{os.sep}foo.csv')
+pivot_districts_filepath_n = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_long_n.csv'
+if not os.path.isfile(pivot_districts_filepath_n):
+    df = pd.read_csv(unpivot_merge_filepath, na_values=['Null','NaN','nan','Nan'])
+    df = df[~df['census_variable'].str.endswith('_pc')]
+    pivot = df.pivot(index=['table', 'census_variable'], columns='district', values='value')
+    pivot.to_csv(pivot_districts_filepath_n, na_rep='Null')
 
-# #ToDo - make sure we remove ranks for any question where sum(question)<2
+pivot_districts_filepath_pc = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_long_pc.csv'
+if not os.path.isfile(pivot_districts_filepath_pc):
+    df = pd.read_csv(unpivot_merge_filepath, na_values=['Null','NaN','nan','Nan'])
+    df = df[df['census_variable'].str.endswith('_pc')]
+    pivot = df.pivot(index=['table', 'census_variable'], columns='district', values='value')
+    pivot.to_csv(pivot_districts_filepath_pc, na_rep='Null')
+
+pivot_districts_filepath = f'{dir}{os.sep}summary{os.sep}2021Census_VIC_SED_2022_long.csv'
+if not os.path.isfile(pivot_districts_filepath):
+    dfp = pd.read_csv(pivot_districts_filepath_pc, na_values=['Null','NaN','nan','Nan'])
+    dfn = pd.read_csv(pivot_districts_filepath_n, na_values=['Null','NaN','nan','Nan'])
+    dfp['type'] = 'proportion'
+    dfn['type'] = 'numeric'
+    df = pd.concat([dfn, dfp], ignore_index=True)
+
+    type = df.pop('type')
+    df.insert(2, 'type', type)
+    
+    df = df.sort_values(['table', 'census_variable', 'type'])
+    df.to_csv(pivot_districts_filepath, na_rep='Null', index=False)
+
+
+# format
+
+df = pd.read_csv(pivot_districts_filepath, na_values=['Null','NaN','nan','Nan'])
+print(df)
